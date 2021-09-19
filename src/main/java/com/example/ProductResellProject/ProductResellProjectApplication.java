925e209688d996b6a5e48bad958a903f1da4e828
jeongsehun <jwk3437@gmail.com>
2021-09-19 14:14:25 +0900

    Initial commit

---
+++ src/main/java/com/example/ProductResellProject/ProductResellProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductResellProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductResellProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductResellProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProductResellProject/ProductResellProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductResellProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductResellProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
