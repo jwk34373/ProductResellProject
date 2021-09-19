c531feb92d6eedee0774d0b4a2d207e2abc2bd8d
dyparkkk <dkdlel1z@naver.com>
2021-09-19 16:42:31 +0900

    ch2 complete

---
+++ src/main/java/com/example/ProductResellProject/web/dto/HelloResponceDto.java
@@ -0,0 +1,13 @@
+package com.example.ProductResellProject.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponceDto {
+
+    private final String name;
+    private final int amount;
+
+}
