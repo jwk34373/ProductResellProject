d3295b048090091be906d2527eacc32d009512f6
dyparkkk <dkdlel1z@naver.com>
2021-09-20 15:32:06 +0900

    ch4_complete

--- src/main/java/com/example/ProductResellProject/service/PostsService.java
+++ src/main/java/com/example/ProductResellProject/service/PostsService.java
@@ -2,12 +2,16 @@ package com.example.ProductResellProject.service;
 
 import com.example.ProductResellProject.domain.posts.Posts;
 import com.example.ProductResellProject.domain.posts.PostsRepository;
+import com.example.ProductResellProject.web.dto.PostsListResponseDto;
 import com.example.ProductResellProject.web.dto.PostsResponseDto;
 import com.example.ProductResellProject.web.dto.PostsSaveRequestDto;
 import com.example.ProductResellProject.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,20 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // (post) -> {PostsListResponseDto(post)}
+                .collect(Collectors.toList());  // stream -> List
+
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/example/ProductResellProject/web/IndexController.java
+++ src/main/java/com/example/ProductResellProject/web/IndexController.java
@@ -1,19 +1,35 @@
 package com.example.ProductResellProject.web;
 
+import com.example.ProductResellProject.service.PostsService;
+import com.example.ProductResellProject.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
