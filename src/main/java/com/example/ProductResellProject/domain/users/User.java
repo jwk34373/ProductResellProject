ea54e6698ffc98d3abb3bab7af5f82a1e30ad467
jeongsehun <jwk3437@gmail.com>
2021-10-11 12:54:19 +0900

    login v3

--- src/main/java/com/example/ProductResellProject/domain/users/User.java
+++ src/main/java/com/example/ProductResellProject/domain/users/User.java
@@ -1,5 +1,6 @@
 package com.example.ProductResellProject.domain.users;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,8 @@ import java.util.Collection;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 public class User implements UserDetails {
 
@@ -31,50 +34,59 @@ public class User implements UserDetails {
     private Role role;
 
 
-
     // 야발 이게 뭐임??? ㅋㅋ
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
 
+    // 사용자의 Password를 반환
     @Override
     public String getPassword() {
         return userPwd;
     }
 
+    // 사용자의 id를 반환
     @Override
     public String getUsername() {
         return userId;
     }
 
+    // 계정 만료 여부 반환
     @Override
     public boolean isAccountNonExpired() {
+        // 이후 만료되었는지 확인하는 로직 구현
         return true;
     }
 
+    // 계정 잠금 여부 반환
     @Override
     public boolean isAccountNonLocked() {
+        // 이후 로직 구현
         return true;
     }
 
+    // 패스워드 만료 여부 반환
     @Override
     public boolean isCredentialsNonExpired() {
+        // 이후 로직 구현
         return true;
     }
 
+    // 계정 사용 가능 여부 반환
     @Override
     public boolean isEnabled() {
+        // 이후 로직 구현
         return false;
     }
 
     //== 생성 메서드 ==//
     @Builder
-    public User(String userId, String userPwd, String name) {
+    public User(String userId, String userPwd, String name, Role role) {
         this.userId = userId;
         this.userPwd = userPwd;
         this.name = name;
-        this.role = Role.ROLE_USER;
+        this.role = role;
     }
 
 
